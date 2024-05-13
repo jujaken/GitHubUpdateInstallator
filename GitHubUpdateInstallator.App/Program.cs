d7be0fb5e532612659c269c9333e3eaa92550472
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-05-13 13:11:02 +0300

    red

--- GitHubUpdateInstallator.App/Program.cs
+++ GitHubUpdateInstallator.App/Program.cs
@@ -1,20 +1,24 @@
 ﻿using GitHubUpdateInstallator.Lib.Services;
+using System.Diagnostics;
 
 #if !DEBUG
 try
 {
 #endif
 
-var dir = Directory.GetCurrentDirectory();
+    var dir = Directory.GetCurrentDirectory();
 
-IInstallatorConfigWorker worker = new InstallatorConfigJsonWorker();
-var cfg = await worker.GetConfig("InstallatorConfig.json");
+    IInstallatorConfigWorker worker = new InstallatorConfigJsonWorker();
+    var cfg = await worker.GetConfig("InstallatorConfig.json");
 
-IAppInfoSevice appInfoService = new AppInfoJsonSevice();
-var update = await appInfoService.GetCurrentUpdate(dir + "/" + "AppUpdate");
+    IAppInfoSevice appInfoService = new AppInfoJsonSevice();
+    var update = await appInfoService.GetCurrentUpdate(dir + "/" + "AppUpdate");
 
-IAppGitHubUpdater appGitHubUpdater = new AppGitHubUpdater();
-await appGitHubUpdater.Download(cfg, update.Version, dir);
+    IAppGitHubUpdater appGitHubUpdater = new AppGitHubUpdater();
+    await appGitHubUpdater.Download(cfg, update.Version, dir);
+
+    Process.Start(args[0]);
+    Environment.Exit(0);
 
 #if !DEBUG
 
